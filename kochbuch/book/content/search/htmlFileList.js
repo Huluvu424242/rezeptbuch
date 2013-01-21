95f8028e722a4fd142525232dab5389d9079b5f9
Thomas Schubert <funthomas424242@gmail.com>
2013-01-21 14:31:12 -0800

    Building site for 1.0-SNAPSHOT

---
+++ kochbuch/book/content/search/htmlFileList.js
@@ -0,0 +1,27 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "apa.html";
+fl["1"]= "ch01.html";
+fl["2"]= "ch01s01.html";
+fl["3"]= "ch01s02.html";
+fl["4"]= "ch01s03.html";
+fl["5"]= "ch02.html";
+fl["6"]= "ch02s01.html";
+fl["7"]= "ch03.html";
+fl["8"]= "ch03s01.html";
+fl["9"]= "ch04.html";
+fl["10"]= "ch04s01.html";
+fl["11"]= "ch05.html";
+fl["12"]= "ch05s01.html";
+fl["13"]= "ch06.html";
+fl["14"]= "ch06s01.html";
+fl["15"]= "ch07.html";
+fl["16"]= "ch07s01.html";
+fl["17"]= "ch07s02.html";
+fl["18"]= "ch08.html";
+fl["19"]= "ch08s01.html";
+fl["20"]= "ch09.html";
+fl["21"]= "ch10.html";
+fl["22"]= "ch10s01.html";
+fl["23"]= "ch10s02.html";
+fl["24"]= "pr01.html";
