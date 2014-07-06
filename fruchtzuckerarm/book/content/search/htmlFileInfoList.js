f8eee71dce9fec8bc90ab04271aed8749594a4ea
Thomas Schubert <funthomas424242@gmail.com>
2014-07-06 03:21:05 -0700

    Building site for 1.0-SNAPSHOT

---
+++ fruchtzuckerarm/book/content/search/htmlFileInfoList.js
@@ -0,0 +1,6 @@
+fil = new Array();
+fil["0"]= "ch02.html@@@Pizza Teig einfach und italienisch@@@null";
+fil["1"]= "pt01.html@@@Aufläufe@@@null";
+fil["2"]= "apa.html@@@Appendix A. Lizenz@@@null";
+fil["3"]= "ch01.html@@@Schnitzelauflauf@@@null";
+fil["4"]= "pt02.html@@@Pizza@@@null";
---
+++ fruchtzuckerarm/book/content/search/htmlFileList.js
@@ -0,0 +1,7 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "ch02.html";
+fl["1"]= "pt01.html";
+fl["2"]= "apa.html";
+fl["3"]= "ch01.html";
+fl["4"]= "pt02.html";
