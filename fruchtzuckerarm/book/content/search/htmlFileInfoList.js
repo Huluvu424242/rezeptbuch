ca28e90d025a70704ee451b71e7a5774db48d932
Thomas Schubert <funthomas424242@gmail.com>
2016-02-12 20:51:24 +0000

    Building site for 1.0-SNAPSHOT

--- fruchtzuckerarm/book/content/search/htmlFileInfoList.js
+++ fruchtzuckerarm/book/content/search/htmlFileInfoList.js
@@ -1,6 +1,6 @@
 fil = new Array();
-fil["0"]= "ch01.html@@@Schnitzelauflauf@@@null";
-fil["1"]= "pt01.html@@@Aufläufe@@@null";
-fil["2"]= "pt02.html@@@Pizza@@@null";
-fil["3"]= "ch02.html@@@Pizza Teig einfach und italienisch@@@null";
-fil["4"]= "apa.html@@@Appendix A. Lizenz@@@null";
+fil["0"]= "pt02.html@@@Pizza@@@null";
+fil["1"]= "apa.html@@@Appendix A. Lizenz@@@null";
+fil["2"]= "ch01.html@@@Schnitzelauflauf@@@null";
+fil["3"]= "pt01.html@@@Aufläufe@@@null";
+fil["4"]= "ch02.html@@@Pizza Teig einfach und italienisch@@@null";
--- fruchtzuckerarm/book/content/search/htmlFileList.js
+++ fruchtzuckerarm/book/content/search/htmlFileList.js
@@ -1,7 +1,7 @@
 //List of files which are indexed.
 fl = new Array();
-fl["0"]= "ch01.html";
-fl["1"]= "pt01.html";
-fl["2"]= "pt02.html";
-fl["3"]= "ch02.html";
-fl["4"]= "apa.html";
+fl["0"]= "pt02.html";
+fl["1"]= "apa.html";
+fl["2"]= "ch01.html";
+fl["3"]= "pt01.html";
+fl["4"]= "ch02.html";
