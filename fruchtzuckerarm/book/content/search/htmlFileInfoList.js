d897e782a30da437a6936aa7cb461e9c63fdd13a
Thomas Schubert <funthomas424242@gmail.com>
2016-05-08 22:21:06 +0000

    Building site for 1.0-SNAPSHOT

--- fruchtzuckerarm/book/content/search/htmlFileInfoList.js
+++ fruchtzuckerarm/book/content/search/htmlFileInfoList.js
@@ -1,6 +1,7 @@
 fil = new Array();
 fil["0"]= "pt02.html@@@Pizza@@@null";
-fil["1"]= "apa.html@@@Appendix A. Lizenz@@@null";
-fil["2"]= "ch01.html@@@Schnitzelauflauf@@@null";
-fil["3"]= "pt01.html@@@Aufläufe@@@null";
-fil["4"]= "ch02.html@@@Pizza Teig einfach und italienisch@@@null";
+fil["1"]= "apa.html@@@Anhang A. Lizenz@@@null";
+fil["2"]= "ch03.html@@@Pizza Teig einfach und italienisch@@@null";
+fil["3"]= "ch01.html@@@Schnitzelauflauf@@@null";
+fil["4"]= "pt01.html@@@Aufläufe@@@null";
+fil["5"]= "ch02.html@@@Überbackener Schupfnudelauflauf@@@null";
--- fruchtzuckerarm/book/content/search/htmlFileList.js
+++ fruchtzuckerarm/book/content/search/htmlFileList.js
@@ -2,6 +2,7 @@
 fl = new Array();
 fl["0"]= "pt02.html";
 fl["1"]= "apa.html";
-fl["2"]= "ch01.html";
-fl["3"]= "pt01.html";
-fl["4"]= "ch02.html";
+fl["2"]= "ch03.html";
+fl["3"]= "ch01.html";
+fl["4"]= "pt01.html";
+fl["5"]= "ch02.html";
