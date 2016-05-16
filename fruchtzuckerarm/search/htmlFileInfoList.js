6fbe0aa2a334540f4458766e8b4aeaa9b0e337a5
Thomas Schubert <funthomas424242@gmail.com>
2016-05-16 16:11:51 +0000

    Building site for 1.0-SNAPSHOT

---
+++ fruchtzuckerarm/search/htmlFileInfoList.js
@@ -0,0 +1,18 @@
+var doStem = true;
+//List of indexed files.
+fl = new Array();
+fl["0"]= "pt02.html";
+fl["1"]= "apa.html";
+fl["2"]= "index.html";
+fl["3"]= "ch03.html";
+fl["4"]= "ch01.html";
+fl["5"]= "pt01.html";
+fl["6"]= "ch02.html";
+fil = new Array();
+fil["0"]= "pt02.html@@@Pizza@@@null";
+fil["1"]= "apa.html@@@Lizenz@@@null";
+fil["2"]= "index.html@@@Fruchtzuckerarm kochen@@@null";
+fil["3"]= "ch03.html@@@Pizza Teig einfach und italienisch@@@null";
+fil["4"]= "ch01.html@@@Schnitzelauflauf@@@null";
+fil["5"]= "pt01.html@@@Aufläufe@@@null";
+fil["6"]= "ch02.html@@@Überbackener Schupfnudelauflauf@@@null";
