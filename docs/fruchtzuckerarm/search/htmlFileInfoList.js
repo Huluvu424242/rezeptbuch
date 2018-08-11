33d48e861b7c4857f00e5626c8a7ca0a2efce038
FunThomas424242 <funthomas424242@gmail.com>
2018-08-11 20:22:15 +0200

    neue generierung von fruchtzuckerarm
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

---
+++ docs/fruchtzuckerarm/search/htmlFileInfoList.js
@@ -0,0 +1,18 @@
+var doStem = true;
+//List of indexed files.
+fl = new Array();
+fl["0"]= "apa.html";
+fl["1"]= "index.html";
+fl["2"]= "ch01.html";
+fl["3"]= "pt02.html";
+fl["4"]= "ch03.html";
+fl["5"]= "ch02.html";
+fl["6"]= "pt01.html";
+fil = new Array();
+fil["0"]= "apa.html@@@Lizenz@@@null";
+fil["1"]= "index.html@@@Fruchtzuckerarm kochen@@@null";
+fil["2"]= "ch01.html@@@Schnitzelauflauf@@@null";
+fil["3"]= "pt02.html@@@Pizza@@@null";
+fil["4"]= "ch03.html@@@Pizza Teig einfach und italienisch@@@null";
+fil["5"]= "ch02.html@@@Überbackener Schupfnudelauflauf@@@null";
+fil["6"]= "pt01.html@@@Aufläufe@@@null";
